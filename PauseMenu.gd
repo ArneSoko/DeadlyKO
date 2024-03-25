838d383f48edf2509922b20cfcb18b677f2b9bd6
Arne Sokolovic <51103803+ArneSoko@users.noreply.github.com>
2024-03-25 11:14:35 -0400

    Fixed pause

--- PauseMenu.gd
+++ PauseMenu.gd
@@ -8,10 +8,10 @@ func _input(_event):
 	if Input.is_action_just_pressed("ui_cancel") and not is_visible():
 		get_tree().paused = true
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-		set_visible(true)
 		back.set_visible(true)
+		set_visible(true)
 		
-	elif Input.is_action_pressed("ui_cancel") and is_visible():
+	elif Input.is_action_just_pressed("ui_cancel") and is_visible():
 		get_tree().paused = false
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 		set_visible(false)
